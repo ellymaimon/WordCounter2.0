bb469ffa449666175ef9344b2d6e99fc8f335751
Elly Maimon <eliranmaimon@gmail.com>
2018-07-06 11:00:10 -0700

    add home controller view test

--- WordCounter.Tests/ControllerTests/HomeControllerTest.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTest.cs
@@ -1,10 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Controllers;
 
 namespace WordCounter.Tests
 {
     [TestClass]
     public class HomeControllerTest
     {
-
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            HomeController controller = new HomeController();
+            ActionResult indexView = controller.Index();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
     }
+
 }
